936ed0d73fde880b80ce963c96d9c7db50d5eace
jacobgronikowski <59928641+jacobgronikowski@users.noreply.github.com>
2021-05-03 08:18:46 -0400

    Initial Commit

---
+++ Check Splitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Check Splitter
+//
+//  Created by Jacob Gronikowski on 5/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
